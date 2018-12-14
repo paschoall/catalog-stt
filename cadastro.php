d7a07c3fbea679e3c2db2c9ce8809b57602d9599
Lucas Turci <lucas.turci@gmail.com>
2018-12-14 11:17:32 -0200

    Adicionei constraint UNIQUE pro email da tabela usuario e fiz cadastro.php cadastrar usuario

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,17 @@
 <?php
+
+
+/* Retirado de: https://stackoverflow.com/questions/4356289/php-random-string-generator*/
+function generateRandomPassword($length = 15) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 /* Retirado de https://www.w3schools.com/php/php_mysql_select.asp */
 
 $servername = "localhost";
@@ -13,21 +26,46 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select * from usuario";
+// Get POST variables
+$email = $_POST['email'];
+$nome = $_POST['nome'];
+if(isset($_POST['senha'])) $senha = $_POST['senha'];
+else $senha = generateRandomString();
+if(isset($_POST['cep'])) $cep = $_POST['cep'];
+if(isset($_POST['rua'])) $rua = $_POST['rua'];
+if(isset($_POST['bairro'])) $bairro = $_POST['bairro'];
+if(isset($_POST['numero'])) $numero = $_POST['numero'];
+if(isset($_POST['cidade'])) $cidade = $_POST['cidade'];
+if(isset($_POST['uf'])) $uf = $_POST['uf'];
+if(isset($_POST['complemento'])) $complemento = $_POST['complemento'];
+
+if(isset($_POST['senha'])) { // cadastro realizado normalmente
+    $sql = "insert into usuario(email, nome, senha, cep, nome_rua, bairro, numero, cidade, estado, complemento) values(" .
+        "'". $email . "'" . ",".
+        "'". $nome . "'" . ",".
+        "'". $senha . "'" . ",".
+        "'". $cep . "'" . ",".
+        "'". $rua . "'" . ",".
+        "'". $bairro . "'" . ",".
+        $numero . "," . //numero nao precisa por entre aspas
+        "'". $cidade . "'" . "," .
+        "'". $uf . "'" . "," .
+        "'". $complemento . "'" .
+        ")";
+} else { // pre-cadastro eh realizado (somente nome email e senha)
+    $sql = "insert into usuario(email, nome, senha) values(" .
+        "'". $email . "'" . ",".
+        "'". $nome . "'" . ",".
+        "'". $senha . "'" .
+        ")";
+}
 
 $result = $conn->query($sql);
 if($result == null) {
     die($conn->error);
 }
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["nome"]. " - Email " . $row["email"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+echo "Cadastro realizado com sucesso";
 
 $conn->close();
 ?> 
--- tables.sql
+++ tables.sql
@@ -1,16 +1,17 @@
 create table usuario(
-    id_user int not null auto_increment primary key, 
+    id_user int not null auto_increment primary key,
     email varchar(45) not null,
     nome varchar(45) not null,
-    senha varchar(255),
-    data_reg datetime,
+    senha varchar(255) not null,
+    data_reg datetime not null default current_timestamp,
     cep varchar(9),
     nome_rua varchar(120),
     bairro varchar(100),
     numero integer,
     cidade varchar(45),
     estado varchar(45),
-    pais varchar(45),
+    pais varchar(45) default 'Brasil',
     complemento varchar(100),
-    admin boolean
+    admin boolean not null default false,
+    UNIQUE(email)
 );
