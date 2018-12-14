cd7db29410e4f8e12e0af449df71f31defd50600
Lucas Turci <lucas.turci@gmail.com>
2018-12-14 00:10:11 -0200

    Adicionei arquivo de criacao das tabelas na base de dados e script php de cadastro com um template de consulta ao banco de dados

---
+++ tables.sql
@@ -0,0 +1,16 @@
+create table usuario(
+    id_user int not null auto_increment primary key, 
+    email varchar(45) not null,
+    nome varchar(45) not null,
+    senha varchar(255),
+    data_reg datetime,
+    cep varchar(9),
+    nome_rua varchar(120),
+    bairro varchar(100),
+    numero integer,
+    cidade varchar(45),
+    estado varchar(45),
+    pais varchar(45),
+    complemento varchar(100),
+    admin boolean
+);
