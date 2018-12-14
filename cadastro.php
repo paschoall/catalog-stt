9418206187c26f2206fde43797879d2cd27c70b2
Lucas Turci <lucas.turci@gmail.com>
2018-12-14 00:34:04 -0200

    Atualizei o template php

--- cadastro.php
+++ cadastro.php
@@ -1,46 +1,33 @@
 <?php
-/* Retirado de https://www.devmedia.com.br/php-e-mysql-conectando-e-exibindo-dados-de-forma-rapida-dica/28526 */
+/* Retirado de https://www.w3schools.com/php/php_mysql_select.asp */
 
-// definições de host, database, usuário e senha
-$host = "localhost";
-$db   = "Catalog_STT";
-$user = "lucas";
-$pass = "lucas123";
-// conecta ao banco de dados
-$con = mysql_pconnect($host, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
-// seleciona a base de dados em que vamos trabalhar
-mysql_select_db($db, $con);
-// cria a instrução SQL que vai selecionar os dados
-$query = sprintf("SELECT identificador, nome, telefone FROM cadastro");
-// executa a query
-$dados = mysql_query($query, $con) or die(mysql_error());
-// transforma os dados em um array
-$linha = mysql_fetch_assoc($dados);
-// calcula quantos dados retornaram
-$total = mysql_num_rows($dados);
-?>
- 
-<html>
-    <head>
-    <title>Exemplo</title>
-</head>
-<body>
-<?php
-    // se o número de resultados for maior que zero, mostra os dados
-    if($total > 0) {
-        // inicia o loop que vai mostrar todos os dados
-        do {
-?>
-            <p><?=$linha['nome']?> / <?=$linha['telefone']?></p>
-<?php
-        // finaliza o loop que vai mostrar os dados
-        }while($linha = mysql_fetch_assoc($dados));
-    // fim do if 
+$servername = "localhost";
+$username = "lucas";
+$password = "lucas123";
+$dbname = "Catalog_STT";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * from usuario";
+
+$result = $conn->query($sql);
+if($result == null) {
+    die($conn->error);
+}
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["nome"]. " - Email " . $row["email"]. "<br>";
     }
-?>
-</body>
-</html>
-<?php
-// tira o resultado da busca da memória
-mysql_free_result($dados);
-?>
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?> 
